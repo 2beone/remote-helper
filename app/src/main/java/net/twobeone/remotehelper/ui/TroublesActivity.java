4ba9f943dd558567bfbed067cb2dd11dff5428ae
gerbugy <gerbugy@naver.com>
2017-06-21 19:06:52 +0900

    commit

--- app/src/main/java/net/twobeone/remotehelper/ui/TroublesActivity.java
+++ app/src/main/java/net/twobeone/remotehelper/ui/TroublesActivity.java
@@ -1,15 +1,49 @@
 package net.twobeone.remotehelper.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 import net.twobeone.remotehelper.R;
 
 public class TroublesActivity extends BaseActivity {
 
+    private static final String TEST_URI = "https://test.webrtc.org";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_troubles);
+
+        // 액션바를 설정합니다.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // 웹뷰를 설정합니다.
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(TEST_URI);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                TroublesActivity.this.runOnUiThread(new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }

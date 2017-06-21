b0c2319717db28596f27ee0404474ad6b8cfacd5
gerbugy <gerbugy@naver.com>
2017-06-21 18:40:28 +0900

    commit

---
+++ app/src/main/java/net/twobeone/remotehelper/ui/BaseFragment.java
@@ -0,0 +1,6 @@
+package net.twobeone.remotehelper.ui;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment {
+}
---
+++ app/src/main/java/net/twobeone/remotehelper/ui/TroublesActivity.java
@@ -0,0 +1,15 @@
+package net.twobeone.remotehelper.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import net.twobeone.remotehelper.R;
+
+public class TroublesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_troubles);
+    }
+}

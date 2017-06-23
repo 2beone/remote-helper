f80a646af253d1a1c505689a6b59bc6a80e933cd
gerbugy <gerbugy@naver.com>
2017-06-23 10:49:33 +0900

    commit

---
+++ app/src/main/java/net/twobeone/remotehelper/MainApp.java
@@ -0,0 +1,11 @@
+package net.twobeone.remotehelper;
+
+import android.app.Application;
+
+public final class MainApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

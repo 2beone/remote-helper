1cae6c32c5e68ba436f04b8c992d91b0762f89ef
gerbugy <gerbugy@naver.com>
2017-06-27 12:09:12 +0900

    commit

--- app/src/main/java/net/twobeone/remotehelper/MainApp.java
+++ app/src/main/java/net/twobeone/remotehelper/MainApp.java
@@ -2,10 +2,19 @@ package net.twobeone.remotehelper;
 
 import android.app.Application;
 
+import net.twobeone.remotehelper.sqlite.SQLiteHelper;
+
 public final class MainApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        SQLiteHelper.initialize(this).getReadableDatabase();
+    }
+
+    @Override
+    public void onTerminate() {
+        SQLiteHelper.getInstance().close();
+        super.onTerminate();
     }
 }
